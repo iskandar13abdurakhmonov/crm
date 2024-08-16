0c786b1a1474bcb1f673e466d61bb668ba4284e0
Iskander <iskandar.13.aburahmonov@gmail.com>
2024-08-16 18:47:41 +1100

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,27 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    compatibilityDate: '2024-04-03',
+    devtools: {enabled: false},
+    modules: [
+        "@nuxt/image",
+        '@nuxtjs/tailwindcss',
+        'shadcn-nuxt',
+        ['@nuxtjs/google-fonts', {
+            families: {
+                Lato: {
+                    wght: [300, 400, 700],
+                    ital: [300]
+                },
+            }
+        }],
+        'nuxt-icon',
+        "@pinia/nuxt"
+    ],
+    shadcn: {
+        prefix: 'Ui',
+        componentDir: './components/ui'
+    },
+    pinia: {
+        storesDirs: ['./store/**']
+    }
+})
